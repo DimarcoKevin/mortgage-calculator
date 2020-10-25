1d6ac2504a0a827be5e676d886df847e8e212614
Kevin <kevin.j.dimarco@gmail.com>
2020-10-24 17:47:07 -0700

    Adding form for mortgage calc

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <form className="Form">
+        <div className="Amount">
+          <label>Mortgage Amount: </label>
+          <input type="number" placeholder="$500000"></input>
+        </div>
+        <div>
+          <label className="Downpayment">Downpayment: </label>
+          <input type="number" placeholder="$25000"></input>
+        </div>
+        <div>
+          <label className="Interest">Interest Rate: </label>
+          <input type="number" placeholder="3.5%"></input>
+        </div>
+        <div>
+          <label className="Years">Length (years): </label>
+          <input type="number" placeholder="25"></input>
+        </div>
+      </form>
     </div>
   );
 }

e9e5d616cd5b9d176c4ef2e4d345af67eea0613a
Kevin <kevin.j.dimarco@gmail.com>
2020-10-25 20:56:01 -0700

    Changing formating and adding useState events

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import { useState } from 'react';
 import './App.css';
 
 
 const App = () => {
 
   const [total, setTotal] = useState(0);
+  const [downpayment, setDownpayment] = useState(0);
+  const [interest, setInterest] = useState(0);
+  const [years, setYears] = useState(0);
 
   const getResult = e => {
 
@@ -23,25 +26,45 @@ const App = () => {
             placeholder="$500000">
           </input>
         </div>
+
         <div>
           <label>Downpayment: </label>
-          <input className="Downpayment" type="number" placeholder="$25000"></input>
+          <input 
+            onChange={event => setDownpayment(event.target.value)}
+            className="Downpayment" 
+            type="number" 
+            placeholder="$25000">
+          </input>
         </div>
+
         <div>
           <label>Interest Rate: </label>
-          <input className="Interest" type="number" placeholder="3.5%"></input>
+          <input 
+            onChange={event => setInterest(event.target.value)}
+            className="Interest" 
+            type="number" 
+            placeholder="3.5%">
+          </input>
         </div>
+
         <div>
           <label>Length (years): </label>
-          <input className="Years" type="number" placeholder="25"></input>
+          <input 
+            onChange={event => setYears(event.target.value)}
+            className="Years" 
+            type="number" 
+            placeholder="25">
+          </input>
         </div>
+
         <div>
           <button className="Clear" type="reset">Clear</button>
           <button className="Submit" type="submit">Submit</button>
         </div>
+
       </form>
       <div className="Result">
-        <h1></h1>
+        
       </div>
     </div>
   );
